f04088e56186b104f96be49662b5f97377a0f621
Frank Fang <FrankFang1990@gmail.com>
2018-10-11 18:56:36 +0800

    add tests-results

--- jest.config.js
+++ jest.config.js
@@ -88,7 +88,8 @@ module.exports = {
   // projects: null,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  "reporters": [ "default", "jest-junit" ],
+
 
   // Automatically reset mock state between every test
   // resetMocks: false,
