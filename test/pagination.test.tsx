68efbc643ab036464345c440434fa6eab628ad6d
chenwenhao <cwenhao88888@gmail.com>
2018-10-23 13:29:00 +0800

    分页组件 测试

---
+++ test/pagination.test.tsx
@@ -0,0 +1,33 @@
+import {configure, mount, ReactWrapper} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import * as React from 'react';
+import Pagination from '../lib/pagination';
+
+configure({adapter: new Adapter()});
+
+describe('Pagination', () => {
+  let c: ReactWrapper
+  const fn = jest.fn()
+  beforeEach(() => {
+    c = mount(
+      <Pagination totalPage={4} currentPage={2} onChange={fn}></Pagination>
+    );
+    console.log(c.html());
+  })
+  it('接受 totalPage', () => {
+    expect(c.find('.gu-pagination-item').length).toEqual(4);
+  });
+  it('接受currentPage', () => {
+    expect(c.find('span[data-name=2]').getDOMNode().classList.contains('gu-pagination-active')).toEqual(true)
+  })
+  it('onChange', () => {
+    c.find('.gu-pagination-prev').simulate('click')
+    expect(fn.mock.calls.length).toEqual(1)
+
+    c.find('.gu-pagination-next').simulate('click')
+    expect(fn.mock.calls.length).toEqual(2)
+
+    c.find('span[data-name=2]').simulate('click')
+    expect(fn.mock.calls.length).toEqual(3)
+  });
+})
