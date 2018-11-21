f72e80e1f7cada48df4ad931abf3ca2c3f3fdc15
chenwenhao <cwenhao88888@gmail.com>
2018-11-21 14:53:39 +0800

    修改 文件名

--- lib/uploader/uploader.tsx
+++ lib/uploader/uploader.tsx
@@ -19,7 +19,7 @@ export interface IProps {
   fileList: IUploadFile[]
 }
 
-export default class Upload extends Component<IProps, IState> {
+export default class Uploader extends Component<IProps, IState> {
   static propTypes = {
     accept: PropTypes.string,
     url: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
@@ -30,7 +30,7 @@ export default class Upload extends Component<IProps, IState> {
     disabled: PropTypes.bool,
     responseParser: PropTypes.func,
     fileList: PropTypes.array
-  }
+  };
 
   public static defaultProps: Partial<IProps> = {
     method: 'POST',
@@ -49,64 +49,58 @@ export default class Upload extends Component<IProps, IState> {
   }
 
   onClickUpload = () => {
-    const input = this.createInput()
+    const input = this.createInput();
     input.addEventListener('change', () => {
-      this.updateFile(input.files![0])
+      this.updateFile(input.files![0]);
       input.remove()
-    })
+    });
     input.click()
-  }
+  };
 
   updateFile = (rawFile: IUploadFile) => {
-    const {name} = rawFile
-    const newName = this.generateName(name)
-    this.beforeUploadFile(rawFile, newName)
-    const formData = new FormData()
-    formData.append(name, rawFile, newName)
-    this.doUploadFile(formData, (response) => {
-      let url = this.parseResponse(response)
-      this.url = url
-      this.afterUploadFile(newName, url)
-    }, () => {
-      this.uploadError(newName)
-    })
-  }
+    const {name} = rawFile;
+    const newName = this.generateName(name);
+    this.beforeUploadFile(rawFile, newName);
+    const formData = new FormData();
+    formData.append(name, rawFile, newName);
+
+  };
 
   beforeUploadFile = (rawFile: IUploadFile, newName: string, url?: string) => {
 
-  }
+  };
 
   generateName = (name: string): string => {
     while (this.props.fileList.filter(f => f.name === name).length > 0) {
-      const dotIndex = name.lastIndexOf('.')
-      const nameWithoutExtension = name.substring(0, dotIndex)
-      const extension = name.substring(dotIndex)
+      const dotIndex = name.lastIndexOf('.');
+      const nameWithoutExtension = name.substring(0, dotIndex);
+      const extension = name.substring(dotIndex);
       name = nameWithoutExtension + '(1)' + extension
     }
     return name
-  }
+  };
 
   createInput = (): HTMLInputElement => {
-    const input = document.createElement('input')
-    input.type = 'file'
+    const input = document.createElement('input');
+    input.type = 'file';
     if (this.props.accept) {
       input.accept = this.props.accept
     }
     if (this.props.multiple) {
       input.multiple = this.props.multiple
     }
-    input.type = 'file'
-    this.inputWrapper!.current!.appendChild(input)
+    input.type = 'file';
+    this.inputWrapper!.current!.appendChild(input);
     return input
-  }
+  };
 
   render() {
-    const {children} = this.props
+    const {children} = this.props;
     const inputStyle = {
       width: 0,
       height: 0,
       overflow: 'hidden'
-    }
+    };
     return (
       <div className={this.sc('wrapper')}>
         <div onClick={this.onClickUpload}>{children}</div>
--- test/uploader.test.tsx
+++ test/uploader.test.tsx
@@ -2,52 +2,52 @@ import {configure, mount, ReactWrapper} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
 
-import GuUpload from '../lib/upload/upload'
+import GuUpload from '../lib/uploader/uploader'
 
 configure({adapter: new Adapter()});
 
-xdescribe('GuUpload 简单属性', () => {
-  let c: ReactWrapper
+xdescribe('GuUploader 简单属性', () => {
+  let c: ReactWrapper;
   beforeEach(() => {
     c = mount(
       <GuUpload>
 
       </GuUpload>
     );
-  })
+  });
 
-  it('接受 accept 属性', () => {
+  it('必填 url,method,inputName 属性', () => {
     expect(c.find('.gu-input').props().value).toEqual('hi')
-  })
+  });
 
   it('接受 url 属性', () => {
 
-  })
+  });
+
+  it('接受 disabled 属性', () => {
+
+  });
 
   it('接受 disabled 属性', () => {
 
   })
+});
 
-  it('接受 disabled 属性', () => {
-
-  })
-})
-
-xdescribe('GuUpload 复杂属性', () => {
-  let c: ReactWrapper
+xdescribe('GuUploader 复杂属性', () => {
+  let c: ReactWrapper;
 
   beforeEach(() => {
     c = mount(
       <GuUpload></GuUpload>
     );
-  })
+  });
 
   it('接受 fileList 属性, 展示图片列表', () => {
 
-  })
+  });
 
   it('onChange 事件获取 file 信息', () => {
 
   })
 
-})
+});
