92a45df61cf95c9861ecbe0c8ffb0467c0b9f708
Frank Fang <FrankFang1990@gmail.com>
2018-12-07 01:30:03 +0800

    声明 FC 类型

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -1,4 +1,12 @@
+import * as React from 'react';
+
 declare module '*.svg' {
   const content: any;
   export default content;
+}
+
+declare global {
+  interface FC<P = {}> extends React.FunctionComponent<P> {
+    displayName: string
+  }
 }
