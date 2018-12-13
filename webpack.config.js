36bcad9783fc73216ca1500f5eebfb0163ed3e37
Frank Fang <FrankFang1990@gmail.com>
2018-12-13 15:33:21 +0800

    成功编译 icon.jsx
    TypeScript 里面不能 importAll ，改成 JS 就可以

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,16 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
       { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
       {
         test: /\.tsx?$/,
