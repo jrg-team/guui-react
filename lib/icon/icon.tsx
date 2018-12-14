2d5cd2194600d9421a6221637876a6543cae02b8
Frank Fang <FrankFang1990@gmail.com>
2018-12-15 02:35:47 +0800

    add icon

---
+++ lib/icon/icon.tsx
@@ -0,0 +1,44 @@
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+import {createScopedClasses, classes} from 'utils/classes';
+import './importAllIcons';
+import * as ReactDOM from 'react-dom';
+
+const componentName = 'Icon';
+const sc = createScopedClasses(componentName);
+
+interface IProps extends IStyledProps {
+  name: string;
+  fill?: string;
+  beforeMount?: (el: Element | null | Text) => void
+  afterMount?: (el: Element | null | Text) => void
+}
+
+class Icon extends React.Component<IProps> {
+  static displayName = componentName;
+  static defaultProps = {
+    fill: 'currentColor'
+  };
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    fill: PropTypes.string,
+  };
+
+  componentWillMount() {
+    this.props.beforeMount && this.props.beforeMount(ReactDOM.findDOMNode(this));
+  }
+
+  componentDidMount() {
+    this.props.afterMount && this.props.afterMount(ReactDOM.findDOMNode(this));
+  }
+
+  render() {
+    return (
+      <svg className={classes(sc('', this.props.name), this.props.className)} style={this.props.style}>
+        <use xlinkHref={`#${this.props.name}`} style={{fill: this.props.fill}}/>
+      </svg>
+    );
+  }
+}
+
+export default Icon;
