a4cdf47bee90c3946ad976eb4eb451239230458e
Frank Fang <FrankFang1990@gmail.com>
2018-12-18 10:38:17 +0800

    add exports

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,6 @@
 import Button from './button/button';
 import Icon from './icon/icon';
+import {Layout, Footer, Aside, Main, Header} from './layout/index'
+import {Col, Row} from './grid/index'
 
-export {Button, Icon};
+export {Button, Icon, Layout, Footer, Aside, Main, Header, Col, Row};
