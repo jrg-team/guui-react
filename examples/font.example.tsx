b9f093cb2e1c23107a66e3eb77f09a7585f1bec0
Frank Fang <FrankFang1990@gmail.com>
2018-12-19 07:27:32 +0800

    update main

---
+++ examples/font.example.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Input from '../lib/input';
+
+export default function (props: any) {
+  return (
+    <div className="ButtonExample">
+      <h2>普通按钮</h2>
+      <div style={{padding: '8px 0'}}>
+        <Input/>
+      </div>
+    </div>
+  );
+}
