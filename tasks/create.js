51bd8e8543493fe6de6eaadb7a865befcfee64ed
Frank Fang <FrankFang1990@gmail.com>
2018-12-28 01:10:35 +0800

    update task

--- tasks/create.js
+++ tasks/create.js
@@ -2,7 +2,7 @@ const p = require('path')
 const fs = require('fs')
 const M = require('mustache')
 const render = M.render.bind(M)
-const customTags = [ '{%', '%}' ];
+const customTags = ['{%', '%}']
 const sourceRoot = p.join(__dirname, '../lib/')
 const exampleRoot = p.join(__dirname, '../examples/')
 const templateRoot = p.join(__dirname, './templates/')
@@ -10,10 +10,10 @@ exports.run = function ({params, options}) {
   const [category, name] = params
   const upperName = upperFirst(name)
   if (category === 'component' || category === 'c') {
-    const filePath = p.join(sourceRoot, `${name}.tsx`)
+    ensureDirSync(p.join(sourceRoot, `${name}/`))
+    const filePath = p.join(sourceRoot, `${name}/${name}.tsx`)
     const examplePath = p.join(exampleRoot, `${name}.example.tsx`)
-    ensureNotFound(filePath)
-    ensureNotFound(examplePath)
+    ensureNotFound(filePath, examplePath)
     write(filePath, fs.readFileSync(p.join(templateRoot, `component.txt`)))
     write(examplePath, fs.readFileSync(p.join(templateRoot, `example.txt`)))
     addRoute()
@@ -24,17 +24,19 @@ exports.run = function ({params, options}) {
     fs.writeFileSync(path, render(data.toString(), {name, upperName}, {}, customTags))
   }
 
-  function ensureNotFound (path) {
-    const exist = fs.existsSync(path)
-    if (exist) {
-      console.error(`Error: file exists! ${path}`)
-      process.exit(1)
-    }
+  function ensureNotFound (...paths) {
+    paths.map(path => {
+      const exist = fs.existsSync(path)
+      if (exist) {
+        console.error(`Error: file exists! ${path}`)
+        process.exit(1)
+      }
+    })
   }
 
   function addExport () {
     const path = p.join(__dirname, '../lib/index.tsx')
-    fs.appendFileSync(path, '\n' + `export {default as ${upperName}} from './${name}';` + '\n', 'utf8')
+    fs.appendFileSync(path, '\n' + `export {default as ${upperName}} from './${name}/${name}';` + '\n', 'utf8')
   }
 
   function addRoute () {
@@ -59,4 +61,9 @@ exports.run = function ({params, options}) {
 
 function upperFirst (string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function ensureDirSync (path) {
+  if (fs.existsSync(path)) { return }
+  fs.mkdirSync(path)
 }
