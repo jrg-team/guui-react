76ce68e206e0e40980e7be73f522761396c9244f
Frank Fang <FrankFang1990@gmail.com>
2019-02-11 16:24:31 +0800

    展示滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -12,6 +12,8 @@ interface IEventHandlers {
 }
 
 interface IState {
+  contentHeight: number;
+  viewHeight: number;
   y: number;
 }
 
@@ -24,11 +26,14 @@ class Scroll extends React.Component<IProps, IState> {
   static propTypes = {};
   private readonly refWrapper: React.RefObject<HTMLDivElement>;
   private readonly refContent: React.RefObject<HTMLDivElement>;
+  private readonly refBar: React.RefObject<HTMLDivElement>;
   private readonly eventHandlers: IEventHandlers;
+
   constructor(props: IProps) {
     super(props);
     this.refWrapper = createRef();
     this.refContent = createRef();
+    this.refBar = createRef();
     this.eventHandlers = {
       onWheel: (e) => {
         this.updateContentY(e.deltaY, () => e.preventDefault());
@@ -37,16 +42,33 @@ class Scroll extends React.Component<IProps, IState> {
         e.preventDefault();
       }
     };
-    this.state = {y: 0};
+    this.state = {
+      y: 0,
+      contentHeight: 0,
+      viewHeight: 0,
+    };
   }
+
   get contentRect() {
     return this.refContent.current!.getBoundingClientRect();
   }
+
   get wrapperRect() {
     return this.refWrapper.current!.getBoundingClientRect();
   }
+
+  get barHeight() {
+    return this.state.viewHeight * this.state.viewHeight / this.state.contentHeight;
+  }
+
+  get barY() {
+    return this.state.y * this.state.viewHeight / this.state.contentHeight;
+  }
+
   get scrollYMax() {
-    if (!this.refContent.current || !this.refWrapper.current) {return 0;}
+    if (!this.refContent.current || !this.refWrapper.current) {
+      return 0;
+    }
     const wrapper = this.refWrapper.current;
     const {borderTopWidth, borderBottomWidth, paddingTop, paddingBottom} = window.getComputedStyle(wrapper);
     const borderTopWidthNumber = parseInt(borderTopWidth!, 10);
@@ -55,6 +77,7 @@ class Scroll extends React.Component<IProps, IState> {
     const paddingBottomNumber = parseInt(paddingBottom!, 10);
     return this.contentRect.height! - this.wrapperRect.height! + (borderTopWidthNumber + borderBottomWidthNumber + paddingTopNumber + paddingBottomNumber);
   }
+
   updateContentY(deltaY: number, callback?: () => void) {
     const y = this.state.y + this.scale(deltaY, 3, 60);
     if (y < 0) {
@@ -66,9 +89,12 @@ class Scroll extends React.Component<IProps, IState> {
       this.setState({y});
     }
   }
+
   scale(n: number, rate: number, limit: number) {
     const abs = Math.abs(n);
-    if (abs < 2) { return n; }
+    if (abs < 2) {
+      return n;
+    }
     const result = n * rate;
     if (abs > limit) {
       return result > 0 ? limit : -limit;
@@ -76,6 +102,17 @@ class Scroll extends React.Component<IProps, IState> {
       return result;
     }
   }
+
+  componentDidMount(): void {
+    const content = this.refContent.current;
+    const wrapper = this.refWrapper.current;
+    if (content && wrapper) {
+      const {height: contentHeight} = content.getBoundingClientRect();
+      const {height: viewHeight} = wrapper.getBoundingClientRect();
+      this.setState({contentHeight, viewHeight});
+    }
+  }
+
   render() {
     return (
       <div ref={this.refWrapper} className={sc()} style={this.props.style} {...this.eventHandlers}>
@@ -84,6 +121,15 @@ class Scroll extends React.Component<IProps, IState> {
           transition: `all 50ms`
         }}>
           {this.props.children}
+        </div>
+        <div className={sc('track')}>
+          <div ref={this.refBar} className={sc('track-bar')}
+               style={{
+                 transform: `translateY(${this.barY}px)`,
+                 height: `${this.barHeight}px`
+               }}>
+            <div className={sc('track-bar-inner')}/>
+          </div>
         </div>
       </div>
     );
