ad5f02098c4d4f97df8c620ac54b2235b0401f12
Zegendary <zxw1992513@qq.com>
2019-02-21 18:03:23 +0800

    完成 slides 组件

--- examples/slides.example.tsx
+++ examples/slides.example.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
 import {Slides} from '../lib/index';
+import {useState} from 'react';
+import './slides.example.scss';
 
 export default function (props: any) {
+  const [current,setCurrent] = useState(2)
   return (
     <div className="SlidesExample">
-      <h2>普通示例</h2>
+      <h2>受控组件</h2>
       <div>
-        <Slides/>
+        <Slides className="sildes-demo" current={current} beforeChange={(value) => setCurrent(value)}>
+          <div className="card"><h3>1</h3></div>
+          <div className="card"><h3>2</h3></div>
+          <div className="card"><h3>3</h3></div>
+          <div className="card"><h3>4</h3></div>
+        </Slides>
+      </div>
+      <h2>非受控组件</h2>
+      <div>
+        <Slides className="sildes-demo" defaultCurrent={1}>
+          <div className="card"><h3>1</h3></div>
+          <div className="card"><h3>2</h3></div>
+          <div className="card"><h3>3</h3></div>
+          <div className="card"><h3>4</h3></div>
+        </Slides>
       </div>
     </div>
   );
--- lib/slides/slides.tsx
+++ lib/slides/slides.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import {createScopedClasses} from 'utils/classes';
+import PropTypes from 'prop-types';
+import './slides.scss';
+import {ReactElement} from 'react';
 
 const componentName = 'Slides';
 const sc = createScopedClasses(componentName);
@@ -11,42 +14,110 @@ interface IProps extends IStyledProps {
   autoplay?: boolean;
   current?: number;
   defaultCurrent?: number;
+  children: Array<ReactElement<any>>;
+  className?: string;
 }
 
 interface IState {
   current?: number;
+  width?: number;
 }
 
 class Slides extends React.Component<IProps, IState> {
   static displayName = componentName;
-  static defaultProps = {};
-  static propTypes = {};
+  static propTypes = {
+    vertical: PropTypes.bool,
+    autoplay: PropTypes.bool,
+    current: PropTypes.number,
+    defaultCurrent: PropTypes.number,
+  };
+
+  private readonly refSlides: React.RefObject<HTMLDivElement>;
+  private refSlidesInner: React.RefObject<HTMLDivElement>;
+  private prev: number;
 
   get current() {
-    return ('defaultCurrent' in this.props ? this.props.current : this.state.current) || 1;
+    return ('defaultCurrent' in this.props ? this.state.current : this.props.current) || 1;
   }
 
   set current(value: number) {
+    this.prev = this.current;
+    this.props.beforeChange && this.props.beforeChange(value, this.prev);
     if ('defaultCurrent' in this.props) {
       this.setState({current: value});
-    } else {
-      this.props.afterChange && this.props.afterChange(value, 1);
     }
+    this.jumpTo(value);
+  }
+
+  jumpTo(value: number): void {
+    this.refSlidesInner.current!.style.transform = `translateX(-${(this.state.width || 0) * (value - 1)}px)`;
+  }
+
+  afterChange(): void {
+    this.props.afterChange && this.props.afterChange(this.current, this.prev);
   }
 
   constructor(props: IProps) {
     super(props);
-    if (!('defaultCurrent' in this.props)) {
+    if ('defaultCurrent' in this.props) {
       this.state = {
-        current: this.props.defaultCurrent
+        current: this.props.defaultCurrent,
+        width: 0
+      };
+    } else {
+      this.state = {
+        width: 0
       };
     }
+    this.refSlides = React.createRef();
+    this.refSlidesInner = React.createRef();
+  }
+
+  componentDidMount(): void {
+    const width = this.refSlides.current!.offsetWidth;
+    this.refSlidesInner.current!.addEventListener('transitionend', this.afterChange.bind(this));
+    this.setState({width}, () => {
+      this.jumpTo(this.current);
+    });
+  }
+
+  componentWillUnmount(): void {
+    this.refSlidesInner.current!.removeEventListener('transitionend', this.afterChange.bind(this));
+  }
+
+  renderNav() {
+    return (
+      <div className={sc('nav')}>
+        {this.props.children
+          .map((c: ReactElement<any>, i) => {
+            const active: string = this.current === i + 1 ? 'active' : '';
+            return <span className={active} onClick={e => this.current = i + 1}
+                         key={i}></span>;
+          })
+        }
+      </div>
+    );
+  }
+
+  renderChildren() {
+    return (
+      <div className={sc('inner')} ref={this.refSlidesInner}
+           style={{width: (this.state.width || 0) * this.props.children.length}}>
+        {
+          this.props.children
+            .map((c: ReactElement<any>, i) => {
+              return <div key={i} className={sc('slide')}>{c}</div>;
+            })
+        }
+      </div>
+    );
   }
 
   render() {
     return (
-      <div className={sc()}>
-        {this.current}
+      <div className={`${sc()} ${this.props.className}`} ref={this.refSlides}>
+        {this.renderChildren()}
+        {this.renderNav()}
       </div>
     );
   }
