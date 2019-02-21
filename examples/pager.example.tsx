5250a606351d2439fe31b9348ad539697e377188
chenwenhao <cwenhao88888@gmail.com>
2019-02-21 12:23:19 +0800

    添加事件

--- examples/pager.example.tsx
+++ examples/pager.example.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import {Pager} from '../lib/index';
+import {useState} from 'react';
 
 export default function (props: any) {
+  const [page,setPage] = useState(5)
+
   return (
     <div className="PagerExample">
       <h2>普通示例</h2>
       <div>
-        <Pager current={1} total={3} />
-        <Pager current={5} total={10} />
+        <Pager current={page} total={10} onChange={value => setPage(value)}/>
       </div>
     </div>
   );
