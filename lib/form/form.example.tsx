4bdbea1cc81eddb16073468f9b361d74ef4fd0cd
frankfang <frankfang1990@gmail.com>
2019-03-31 03:20:48 +0800

    show errors

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,24 +1,43 @@
 import * as React from 'react';
 import {Form} from '..';
 import {useState} from 'react';
-import {FormField} from './form';
+import {FormField, FormValue} from './form';
+
+const validate = (formValue: FormValue) => {
+  if (formValue.name.length < 3) {
+    return {name: ['太短']};
+  } else {
+    return {};
+  }
+};
 
 export default function (props: any) {
-  const [formData] = useState({
+  const [formData, setFormData] = useState<FormValue>({
     name: 'frank',
     age: 18,
     password: 'xxxxx'
   });
   const [fields] = useState<FormField[]>([
-    {name: 'name', label: '姓名', type: 'text'},
-    {name: 'age', label: '年龄', type: 'number'},
-    {name: 'password', label: '密码', type: 'password'},
+    {name: 'name', render: {label: '姓名', type: 'text'}},
+    {name: 'age', render: {label: '年龄', type: 'number'}},
+    {name: 'password', render: {label: '密码', type: 'password'}},
   ]);
+  const onChange = (value: FormValue) => {
+    setFormData(value);
+    const errors = validate(value);
+    setErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      console.log('valid');
+    } else {
+      console.log('not valid');
+    }
+  };
+  const [errors, setErrors] = useState<any>({});
   return (
     <div className="FormExample">
       <h2>普通示例</h2>
       <div>
-        <Form dataSource={formData} fields={fields}/>
+        <Form value={formData} fields={fields} onChange={onChange} errors={errors}/>
       </div>
     </div>
   );
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,37 +1,60 @@
 import * as React from 'react';
 import {createScopedClasses} from 'utils/classes';
+import {Input} from '../index';
+import {ReactNode} from 'react';
 
 const componentName = 'Form';
 const sc = createScopedClasses(componentName);
 
-export interface FormField {
-  name: string;
+interface FormFieldDefaultRender {
   label: string;
   type: 'text' | 'textarea' | 'number' | 'password';
 }
 
+export interface FormField {
+  name: string;
+  render: (() => ReactNode) | FormFieldDefaultRender;
+}
+
+export interface FormValue {
+  [K: string]: any;
+}
+
+export interface FormErrors {
+  [K: string]: string;
+}
+
 interface IProps extends IStyledProps {
-  dataSource: { [K: string]: any };
+  value: FormValue;
   fields: FormField[];
+  errors: FormErrors;
+  onChange: (value: FormValue) => void;
 }
 
-interface IState {
-}
+const Form: React.FunctionComponent<IProps> = (props) => {
+  const onInputChange = (name: string, e: any) => {
+    props.onChange({...props.value, [name]: e.target.value});
+  };
+  const fields = props.fields.map((f: FormField) =>
+    <div key={f.name}>
+      {f.render instanceof Function ?
+        f.render() :
+        <Input value={props.value[f.name]}
+               error={props.errors[f.name]}
+               onChange={onInputChange.bind(null, f.name)}/>}
+    </div>
+  );
+  return (
+    <div className={sc('wrapper')}>
+      <form>
+        {fields}
+      </form>
+    </div>
+  );
+};
 
-class Form extends React.Component<IProps, IState> {
-  static displayName = componentName;
-  static defaultProps = {};
-  static propTypes = {};
-
-  constructor(props: IProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={sc()}>TO DO</div>
-    );
-  }
-}
+Form.displayName = componentName;
+Form.defaultProps = {};
+Form.propTypes = {};
 
 export default Form;
