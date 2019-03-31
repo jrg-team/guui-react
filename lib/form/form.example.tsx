ec436228e880f01aa702174d787a6474f7eeaa6c
frankfang <frankfang1990@gmail.com>
2019-03-31 17:41:07 +0800

    add form

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Form} from '..';
-import {useState} from 'react';
+import {Button, Form} from '..';
+import {useState, Fragment} from 'react';
 import {FormField, FormValue} from './form';
 
 const validate = (formValue: FormValue) => {
@@ -18,27 +18,42 @@ export default function (props: any) {
     password: 'xxxxx'
   });
   const [fields] = useState<FormField[]>([
-    {name: 'name', render: {label: '姓名', type: 'text'}},
-    {name: 'age', render: {label: '年龄', type: 'number'}},
-    {name: 'password', render: {label: '密码', type: 'password'}},
+    {name: 'name', label: '用户名', input: {type: 'text'}},
+    {name: 'age', label: '年龄', input: {type: 'number'}},
+    {name: 'password', label: '密码', input: {type: 'password'}},
   ]);
   const onChange = (value: FormValue) => {
     setFormData(value);
     const errors = validate(value);
     setErrors(errors);
-    if (Object.keys(errors).length === 0) {
-      console.log('valid');
-    } else {
-      console.log('not valid');
-    }
   };
   const [errors, setErrors] = useState<any>({});
+  const onSubmit = () => {
+    console.log(formData);
+  };
   return (
     <div className="FormExample">
       <h2>普通示例</h2>
-      <div>
-        <Form value={formData} fields={fields} onChange={onChange} errors={errors}/>
-      </div>
+      <Form value={formData} fields={fields} onChange={onChange} errors={errors}
+            onSubmit={onSubmit}
+            buttons={
+              <Fragment>
+                <Button type="submit" level="important">提交</Button>
+                <Button>返回</Button>
+              </Fragment>
+            }
+      />
+      <h2>普通示例</h2>
+      <Form value={formData} fields={fields} onChange={onChange} errors={errors}
+            layout="vertical"
+            onSubmit={onSubmit}
+            buttons={
+              <Fragment>
+                <Button type="submit" level="important">提交</Button>
+                <Button>返回</Button>
+              </Fragment>
+            }
+      />
     </div>
   );
 }
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import {createScopedClasses} from 'utils/classes';
 import {Input} from '../index';
-import {ReactNode} from 'react';
+import {FormEvent, ReactNode} from 'react';
+import './form.scss';
 
 const componentName = 'Form';
 const sc = createScopedClasses(componentName);
 
 interface FormFieldDefaultRender {
-  label: string;
   type: 'text' | 'textarea' | 'number' | 'password';
 }
 
 export interface FormField {
   name: string;
-  render: (() => ReactNode) | FormFieldDefaultRender;
+  label: string;
+  input: (() => ReactNode) | FormFieldDefaultRender;
 }
 
 export interface FormValue {
@@ -25,9 +26,12 @@ export interface FormErrors {
 }
 
 interface IProps extends IStyledProps {
+  layout?: 'vertical' | 'horizontal' | 'inline';
   value: FormValue;
   fields: FormField[];
   errors: FormErrors;
+  buttons: ReactNode;
+  onSubmit: (value: FormValue) => void;
   onChange: (value: FormValue) => void;
 }
 
@@ -35,26 +39,78 @@ const Form: React.FunctionComponent<IProps> = (props) => {
   const onInputChange = (name: string, e: any) => {
     props.onChange({...props.value, [name]: e.target.value});
   };
-  const fields = props.fields.map((f: FormField) =>
-    <div key={f.name}>
-      {f.render instanceof Function ?
-        f.render() :
-        <Input value={props.value[f.name]}
-               error={props.errors[f.name]}
-               onChange={onInputChange.bind(null, f.name)}/>}
-    </div>
+  const renderInput = (field: FormField) =>
+    <div className={sc('input-wrapper')} key={field.name}>
+      {field.input instanceof Function ?
+        field.input() :
+        <Input value={props.value[field.name]}
+               onChange={onInputChange.bind(null, field.name)}/>}
+    </div>;
+
+  const horizontalLayout = (
+    <table className={sc('table')}>
+      <tbody>
+      {props.fields.map(f =>
+        <tr key={f.name} className={sc('tr')}>
+          <td className={sc('td')}>
+            <div className={sc('label')}>{f.label}</div>
+          </td>
+          <td className={sc('td')}>
+            {renderInput(f)}
+          </td>
+          <td className={sc('td')}>
+            {props.errors[f.name] &&
+            <div className={sc('error')}>{props.errors[f.name]}</div>}
+          </td>
+        </tr>
+      )}
+      <tr>
+        <td className={sc('td')}/>
+        <td className={sc('td')} colSpan={2}>
+          <div className={sc('buttons')}>
+            {props.buttons}
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   );
+  const verticalLayout = <div>
+    {props.fields.map(f =>
+      <div key={f.name} className={sc('row')}>
+        <div className={sc('label')}>{f.label}</div>
+        {renderInput(f)}
+        {props.errors[f.name] &&
+        <div className={sc('error')}>
+          {props.errors[f.name]}
+        </div>
+        }
+      </div>
+    )}
+    <div className={sc('buttons')}>
+      {props.buttons}
+    </div>
+  </div>;
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    props.onSubmit(props.value);
+  };
   return (
     <div className={sc('wrapper')}>
-      <form>
-        {fields}
+      <form className={sc('', props.layout)} onSubmit={onSubmit}>
+        {props.layout === 'horizontal' ?
+          horizontalLayout :
+          verticalLayout
+        }
       </form>
     </div>
   );
 };
 
 Form.displayName = componentName;
-Form.defaultProps = {};
+Form.defaultProps = {
+  layout: 'horizontal'
+};
 Form.propTypes = {};
 
 export default Form;
